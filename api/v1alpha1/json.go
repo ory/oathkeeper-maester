8f048680168ba0201b7e3b51670f78e5dd466820
aeneasr <aeneas@ory.sh>
2020-02-18 15:21:54 +0100

    fix: Do not escape < > in jsonify
    Closes #38

---
+++ api/v1alpha1/json.go
@@ -0,0 +1,31 @@
+package v1alpha1
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+func unescapedMarshalIndent(in interface{}, prefix, indent string) ([]byte, error) {
+	var b bytes.Buffer
+	enc := json.NewEncoder(&b)
+	enc.SetEscapeHTML(false)
+	enc.SetIndent(prefix, indent)
+	if err := enc.Encode(in); err != nil {
+		return nil, err
+	}
+
+	result := b.Bytes()
+	return result[:len(result)-1], nil
+}
+
+func unescapedMarshal(in interface{}) ([]byte, error) {
+	var b bytes.Buffer
+	enc := json.NewEncoder(&b)
+	enc.SetEscapeHTML(false)
+	if err := enc.Encode(in); err != nil {
+		return nil, err
+	}
+
+	result := b.Bytes()
+	return result[:len(result)-1], nil
+}
