987c91d74e9c6ee25cfc2397dc1f0ef0bb8b0154
Jakub Błaszczyk <daemonsthere@gmail.com>
2022-06-23 10:00:53 +0200

    feat: add bearer_token (#58)

---
+++ api/v1alpha1/defaults.go
@@ -0,0 +1,14 @@
+package v1alpha1
+
+var (
+	DefaultAuthenticatorsAvailable = [...]string{"noop", "unauthorized", "anonymous", "cookie_session", "oauth2_client_credentials", "oauth2_introspection", "jwt", "bearer_token"}
+	DefaultAuthorizersAvailable    = [...]string{"allow", "deny", "keto_engine_acp_ory"}
+	DefaultMutatorsAvailable       = [...]string{"noop", "id_token", "header", "cookie", "hydrator"}
+)
+
+const (
+	AuthenticatorsAvailableEnv = "authenticatorsAvailable"
+	AuthorizersAvailableEnv    = "authorizersAvailable"
+	MutatorsAvailableEnv       = "mutatorsAvailable"
+	RulesFileNameRegexp        = "\\A[-._a-zA-Z0-9]+\\z"
+)
--- go.mod
+++ go.mod
@@ -12,10 +12,14 @@ require (
 	github.com/onsi/gomega v1.10.2
 	github.com/stretchr/testify v1.6.1
 	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	k8s.io/api v0.20.2
 	k8s.io/apimachinery v0.20.2
 	k8s.io/client-go v0.20.2
 	sigs.k8s.io/controller-runtime v0.8.3
+)
+
+replace (
+	github.com/prometheus/client_golang => github.com/prometheus/client_golang v1.12.2
+	google.golang.org/protobuf => google.golang.org/protobuf v1.28.0
 )
