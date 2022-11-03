684dae8f500130646523f18a4a2b5a9258e5a3ec
Kevin Goslar <kevin.goslar@gmail.com>
2022-11-03 10:34:00 -0400

    docs: standardize license headers (#63)

--- api/v1alpha1/rule_types_test.go
+++ api/v1alpha1/rule_types_test.go
@@ -1,28 +1,17 @@
-/*
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright © 2022 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
 
 package v1alpha1
 
 import (
 	"fmt"
+	"testing"
+
 	"github.com/ory/oathkeeper-maester/internal/validation"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"testing"
 )
 
 var (
