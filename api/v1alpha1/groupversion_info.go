bbba533384f48654e6b459eb47dfbd8c3e479bf7
Leandro Batista <lbatista@lemongrasscloud.com>
2023-04-25 10:56:17 -0300

    formatting

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,4 +1,4 @@
-// Copyright © 2022 Ory Corp
+// Copyright © 2023 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
 
 // Package v1alpha1 contains API Schema definitions for the oathkeeper v1alpha1 API group
